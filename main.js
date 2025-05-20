0e45aca78af2b312020682a2b8fd67300e4aba56
lugzf <lucgzf@gmail.com>
2025-05-19 21:58:13 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,32 +1,32 @@
-import { PlaywrightCrawler, Actor } from 'crawlee';
+import { PlaywrightCrawler, log, Dataset } from 'crawlee';
 
-await Actor.init();
-
-const input = await Actor.getInput() || { startUrls: [] };
+const input = await JSON.parse(process.env.INPUT || '{"startUrls": []}');
 
 const crawler = new PlaywrightCrawler({
     requestHandlerTimeoutSecs: 60,
 
-    async requestHandler({ page, request, log, pushData }) {
+    async requestHandler({ page, request }) {
         log.info(`Abrindo página: ${request.url}`);
+
         await autoScroll(page);
         await page.waitForTimeout(3000);
+
         const content = await page.content();
         const match = content.match(/(\d+)\s+quartos?/i);
         const hotelSize = match ? match[0] : null;
-        await pushData({
+
+        await Dataset.pushData({
             url: request.url,
             hotelSize,
         });
     },
 
-    failedRequestHandler({ request, log }) {
+    failedRequestHandler({ request }) {
         log.error(`Falha ao acessar ${request.url}`);
     }
 });
 
 await crawler.run(input.startUrls);
-await Actor.exit();
 
 async function autoScroll(page) {
     await page.evaluate(async () => {
